5ca635868a5e4ddc49f0d06578400fe25a9da1ef
Carlinhos de Sousa Junior <cdesousajunior@gmail.com>
2018-04-27 22:02:40 -0300

    Criação da classe Rbc

---
+++ Rbc/Models/Rbc.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rbc.Models
+{
+    public static class Rbc
+    {
+
+        //calculo da similaridade -> (valor da soma das variaveis/soma dos pesos das variaveis) = score
+
+        public static double ConsultarSimilaridade<TCaso>(TCaso caso) where TCaso : Caso // <- passar os dois casos a serem comparados
+        {
+            // Chamar aqui -> ConsultarSimilaridadeParaTiposNumericos();
+            return 0.0;
+        }
+
+        /// <summary>
+        /// Consulta similaridade para os tipos numéricos.
+        /// </summary>
+        /// <param name="v1c1">Valor 1 do caso 1</param>
+        /// <param name="v1c2">Valor 1 do caso 2</param>
+        /// <returns></returns>
+        private static double ConsultarSimilaridadeParaTiposNumericos(int v1c1, int v1c2, int intervaloMin, int intervaloMax)
+        {
+            return 1 - Math.Abs(v1c2 - v1c1) / (intervaloMax - intervaloMin);
+        }
+
+        /*
+         - Ordenar os itens com base no score.
+         */
+    }
+}
